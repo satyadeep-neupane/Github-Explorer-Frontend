61c7afbf3b25abed9684f845f1ce4a12388b13d3
satyadeep-neupane <satyadeep.neupane@deerwalk.edu.np>
2024-06-06 16:38:53 +0545

    Design completed

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,20 @@ import { IoSearch } from "react-icons/io5";
 
 export default function App() {
     return (
-        <div class="container mx-auto h-screen bg-custom-black">
+        <div className="container-full mx-auto h-screen bg-custom-black">
             <h1 className="text-custom-white text-6xl font-semibold text-center pt-[7rem]">
                 GitHub Explorer
             </h1>
             <form>
-                <div class="mt-7">
-                    <div class="relative w-5/12 mx-auto">
-                        <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
+                <div className="mt-7">
+                    <div className="relative w-10/12 md:w-8/12 lg:w-6/12 max-w-2xl mx-auto">
+                        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
                             <IoSearch className="text-white" />
                         </span>
                         <input
                             name="q"
                             type="search"
-                            class="pl-10 w-full bg-transparent border-white border-2 px-3.5 py-2 focus:border-gray-500 focus:ring-0 rounded-md shadow-sm text-white"
+                            className="pl-10 w-full bg-transparent border-white border-2 px-3.5 py-2 focus:border-gray-500 focus:ring-0 rounded-md shadow-sm text-white"
                             placeholder="Search Github"
                         />
                     </div>
