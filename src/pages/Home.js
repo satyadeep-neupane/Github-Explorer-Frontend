abda579f51f643e5e1f81f183aad07fe17cc7d74
satyadeep-neupane <satyadeep.neupane@deerwalk.edu.np>
2024-06-07 13:19:40 +0545

    Home Page Data handling done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,24 @@
+import { useCallback, useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+    const navigate = useNavigate();
+
+    const [searchString, setSearchString] = useState("");
+
+    const handleSearchChange = useCallback((e) => {
+        setSearchString(e.target.value);
+    }, []);
+
+    const handleSubmit = useCallback(
+        (e) => {
+            e.preventDefault();
+            navigate(`/search?q=${searchString}`);
+        },
+        [navigate, searchString]
+    );
+
     return (
         <main className="container-full mx-auto h-screen bg-custom-black">
             <header className="text-center pt-[7rem]">
@@ -9,7 +27,7 @@ export default function Home() {
                 </h1>
             </header>
             <section>
-                <form aria-label="Search GitHub">
+                <form aria-label="Search GitHub" onSubmit={handleSubmit}>
                     <div className="mt-7">
                         <div className="relative w-10/12 md:w-8/12 lg:w-6/12 max-w-2xl mx-auto">
                             <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
@@ -17,6 +35,8 @@ export default function Home() {
                             </span>
                             <input
                                 name="q"
+                                value={searchString}
+                                onChange={handleSearchChange}
                                 type="search"
                                 className="pl-10 w-full bg-transparent border-white border-2 px-3.5 py-2 focus:border-gray-500 focus:ring-0 rounded-md shadow-sm text-white"
                                 placeholder="Search GitHub"
