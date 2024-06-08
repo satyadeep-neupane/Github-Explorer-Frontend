f1c411740de94c0460c05898590a6cbe60d72564
satyadeep-neupane <satyadeep.neupane@deerwalk.edu.np>
2024-06-08 11:24:08 +0545

    Markdown css-bug corrected

--- src/components/MarkdownSection.jsx
+++ src/components/MarkdownSection.jsx
@@ -6,7 +6,7 @@ function MarkdownSection({ markdown }) {
         <section className="text-white border-gray-400 border-x border-b rounded-b-lg py-3 px-4">
             <h3 className="text-2xl text-zinc-400 mb-2">Readme</h3>
             <hr className="mb-2" />
-            <Markdown className="prose prose-slate dark:prose-invert lg:prose-sm">
+            <Markdown className="prose prose-slate prose-invert lg:prose-sm">
                 {markdown}
             </Markdown>
 
